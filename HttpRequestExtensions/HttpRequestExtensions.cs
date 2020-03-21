e7258d18cae44934fd01b34f21bb7d25d6f63203
Denis Goncharenko <denis.goncharenko@acropolium.com>
2020-03-21 19:53:21 +0200

    add source code, gitignore. update readme

---
+++ HttpRequestExtensions/HttpRequestExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace HttpRequestExtensions
+{
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Create a Absolute Uri from Request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Absolute path as Uri object</returns>
+        public static Uri GetAbsoluteUri(this HttpRequest request)
+        {
+            var url = $"{request.Scheme}://{request.Host.Host}:{request.Host.Port}{request.Path}";
+            Uri.TryCreate(url, UriKind.Absolute, out var uri);
+            return uri;
+        }
+
+        /// <summary>
+        /// Create a Absolute Uri with QueryString from Request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Absolute path as Uri object</returns>
+        public static Uri GetAbsoluteUriWithQueryString(this HttpRequest request)
+        {
+            var url = $"{request.Scheme}://{request.Host.Host}:{request.Host.Port}{request.Path}{request.QueryString}";
+            Uri.TryCreate(url, UriKind.Absolute, out var uri);
+
+            return uri;
+        }
+
+        /// <summary>
+        /// Create a relative path with query string.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Relative path with query string</returns>
+        public static string GetRelativePathWithQueryString(this HttpRequest request)
+        {
+            return $"{request.Path}{request.QueryString}";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # HttpRequestExtensions
+
+The HttpRequestExtensions is a [Nuget Package](https://www.nuget.org/packages/UrlCombine) for getting absolute or relative url.
+This library return absolute and relative url from HttpRequest.
+
+## Usages
+
+### Add using
+``` csharp
+  using HttpRequestExtensions;
+```
+
+### Examples
+
+#### Get Absolute Uri
+
+``` csharp
+    HttpContext.Request.GetAbsoluteUri();
+```
+or
+``` csharp
+    HttpContextExtensions.GetAbsoluteUri(HttpContext.Request);
+```
+
+#### Get Absolute Uri with Query String
+
+``` csharp
+    HttpContext.Request.GetAbsoluteUriWithQueryString();
+```
+or
+``` csharp
+    HttpContextExtensions.GetAbsoluteUriWithQueryString(HttpContext.Request);
+```
+
+#### Get Relative Path with Query String
+
+``` csharp
+    HttpContext.Request.GetRelativePathWithQueryString();
+```
+or
+``` csharp
+    HttpContextExtensions.GetRelativePathWithQueryString(HttpContext.Request);
+```
